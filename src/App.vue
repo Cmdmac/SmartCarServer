41cb6c2dfbc5f8812542952fededdeccc2751ca7
cmdmac <cmdmac@126.com>
2024-07-22 21:57:41 +0800

    add ws send data

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <template>
 <!--  <img alt="Vue logo" src="./assets/logo.png">-->
 <!--  <HelloWorld msg="Welcome to Your Vue.js App"/>-->
-  <ElButton @click="handleClick">前进</ElButton>
-  <ElButton>后退</ElButton>
-  <ElButton>左转</ElButton>
-  <ElButton>右转</ElButton>
+  <ElButton @click="onForward">前进</ElButton>
+  <ElButton @click="onBackward">后退</ElButton>
+  <ElButton @click="onTurnLeft">左转</ElButton>
+  <ElButton @click="onTurnRight">右转</ElButton>
   <el-button>test</el-button>
 </template>
 
@@ -22,12 +22,21 @@ export default {
     }
   },
   methods: {
-    handleClick() {
-      alert("hello")
-    }
+    onForward() {
+      this.socket.send(JSON.stringify({command: 0}));
+    },
+    onBackward() {
+      this.socket.send(JSON.stringify({command: 1}));
+    },
+    onTurnLeft() {
+      this.socket.send(JSON.stringify({command: 2}));
+    },
+    onTurnRight() {
+      this.socket.send(JSON.stringify({command: 3}));
+    },
   },
   created() {
-    this.socket = new WebSocket('ws://localhost:3000/ws');
+    this.socket = new WebSocket('ws://localhost:3000/web');
 
     this.socket.onopen = () => {
       console.log('WebSocket 连接已打开');
@@ -36,6 +45,7 @@ export default {
     this.socket.onmessage = (event) => {
       this.message = event.data;
       console.log(event.data);
+      console.log(JSON.stringify(event.data.command))
     };
 
     this.socket.onerror = (error) => {
